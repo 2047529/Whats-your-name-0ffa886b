484d18106cbd92e608749fc5d14daa44942113d0
Arjen Bos <2047529@talnet.nl>
2019-09-30 16:44:15 +0200

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,11 @@
+<?php
+
+$voornaam = "Hallo! Wat is uw voornaam?\n";
+echo $voornaam;
+$vn = readline();
+$achternaam = "Wat is uw achternaam?\n";
+echo $achternaam;
+$an = readline();
+echo $vn . " " . $an;
+
+?>
